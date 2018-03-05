35a400d9e775cd7ef38bf68e935633b001289500
Andrew <awmarsden487@gmail.com>
2018-03-05 15:21:56 -0500

    removed duplicate of ClubConnexions. I think I set it up wrong when I first started doing it

--- ClubConnexions/calendarPage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalendarpageConfig(AppConfig):
-    name = 'calendarPage'
--- ClubConnexions/home/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Class for holding the description on the home page
-class Description(models.Model):
-    descriptionText = models.CharField(max_length = 400)
