6b9afba367f7d8f9250f6a2045a65f6f126aaae6
Andrew <awmarsden487@gmail.com>
2018-03-13 21:49:27 -0400

    Followed Django Tutorial closely. Updated Home Page so that it uses HTML instead of hard coded Django.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Description
 
 # Create your views here.
 #Instructions text will show up on the webpage but will be hard to read since
@@ -13,4 +16,9 @@ from django.http import HttpResponse
 
 #Testing Stuff cuz GIT HUB IS SO ANNOYING
 def index(request):
-    return HttpResponse("Welcome to the Home Page\nTo get to Calendar, go to http://localhost:8000/calendar/\nTo get to Documents, go to http://localhost:8000/documents/\nTo get to Officer Info, go to http://localhost:8000/officers/\nTo get to Club Members, go to http://localhost:8000/members/\nTo get to Admin Login, go to http://localhost:8000/login/")
+    desc = Description.descriptionText
+    template = loader.get_template('home/index.html')
+    context = {
+        'desc':desc,
+    }
+    return HttpResponse(template.render(context, request))
