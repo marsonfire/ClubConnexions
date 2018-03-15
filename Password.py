92150143389a721a8264c2b19b24668173df6a60
Scott <scottpatharrington@gmail.com>
2018-03-15 19:44:44 -0400

    updated

--- Password.py
+++ Password.py
@@ -17,22 +17,22 @@ class Password:
 # take an input password and sets it to be the current password
 # pre - currentPassword is whate ever it currently is 
 # post - currentPassword = what ever password was
-    def createPassword(password):
-        currentPassword = password
-        return
+    def createPassword(self, password):
+        self.currentPassword = password
+        return password
         
 #edit password
 # edits the current password 
 # pre - admin selects edit password
 # post-  password has been edited to newPassword 
-    def editPassword(currentPasswordCheck, newPassword, newPasswordCheck):
+    def editPassword(self, currentPasswordCheck, newPassword, newPasswordCheck):
 #Checks to see if admin is enabled
-        if(boolAdmin == True):
+        if(self.boolAdmin == True):
 #Checks to make sure admin is admin when changing password
-            if(currentPasswordCheck == currentPassword):
+            if(currentPasswordCheck == self.currentPassword):
 # Checks to see if first enterd password is the same as the second entered password       
                 if(newPassword == newPasswordCheck):
-                    createPassword(newPassword)# set the new password to be the current password
+                   return self.createPassword(newPassword)# set the new password to be the current password
                 else:
                     return
             else:
@@ -43,9 +43,10 @@ class Password:
 # enables admin for the person logged in
 #pre - user not logged in 
 #post- person is logged in as admin
-    def enableAdmin(passwordEntered):
-        if (passwordEntered == currentPassword):
-            boolAdmin = True
-            return
+    def enableAdmin(self, passwordEntered):
+        if (passwordEntered == self.currentPassword):
+             self.boolAdmin = True
+             return True
+            
         else:
-            return
+            return False
