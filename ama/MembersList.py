da11ddc214526c583cf6ad3a41502175752a5610
Edwin <edwin.j.cerv@gmail.com>
2018-03-19 17:14:26 -0400

    Added Class Member to Member List to not have so much confusion.

--- ama/MembersList.py
+++ ama/MembersList.py
@@ -2,7 +2,26 @@
 #the whole list of members
 import unittest
 import operator
-from MemberClass import Member
+
+class Member(object):
+	allCardNumber = 0
+	lastName = ""
+	firstName = ""
+	gradYear = ""
+	position = ""
+	email = ""
+	major = ""
+	minor = ""
+	def __init__(self, allCardNumber, lastName, firstName, gradYear, position, email, major, minor):
+		self.allCardNumber = allCardNumber
+		self.lastName = lastName
+		self.firstName = firstName
+		self.gradYear = gradYear
+		self.position = position
+		self.email = email
+		self.major = major
+		self.minor = minor
+	
 
 memberList = {}#This is our list in the form of a dictionary
 
@@ -11,11 +30,12 @@ memberList = {}#This is our list in the form of a dictionary
 
 #To add/remove to the list we will have the all card number as the key and MemberClass(obj) is the value
 
-def addToList(self, allCardNumber, MemberClass):
-	memberList[allCardNumber] = MemberClass
+def addToList(self, allCardNumber, lastName, firstName, gradYear, position, email, major, minor):
+	member = Member(allCardNumber, lastName, firstName, gradYear, position, email, major, minor)
+	memberList[allCardNumber] = member
 
 def removeFromList(self, allCardNumber):
 	del memberList[allCardNumber]
 
 def printList(self):
-	print memberList
+	print memberList	
