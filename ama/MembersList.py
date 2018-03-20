658a4d12b5def65b51f7c332b7adc2c7218a8ec9
Edwin <edwin.j.cerv@gmail.com>
2018-03-20 15:04:28 -0400

    Added way of retrieving information from a member who was added into the dictionary. Then added a simple unit test making sure all methods worked correctly.

--- ama/MembersList.py
+++ ama/MembersList.py
@@ -7,24 +7,24 @@ memberList = {}#This is our list in the form of a dictionary
 
 
 class Member(object):
-	allCardNumber = 0
-	lastName = ""
-	firstName = ""
-	gradYear = ""
-	position = ""
-	email = ""
-	major = ""
-	minor = ""
-	def __init__(self, allCardNumber, lastName, firstName, gradYear, position, email, major, minor):
-		self.allCardNumber = allCardNumber
-		self.lastName = lastName
-		self.firstName = firstName
-		self.gradYear = gradYear
-		self.position = position
-		self.email = email
-		self.major = major
-		self.minor = minor
-	
+        allCardNumber = 0
+        lastName = ""
+        firstName = ""
+        gradYear = ""
+        position = ""
+        email = ""
+        major = ""
+        minor = ""
+        def __init__(self, allCardNumber, lastName, firstName, gradYear, position, email, major, minor):
+                self.allCardNumber = allCardNumber
+                self.lastName = lastName
+                self.firstName = firstName
+                self.gradYear = gradYear
+                self.position = position
+                self.email = email
+                self.major = major
+                self.minor = minor
+        
 
 memberList = {}#This is our list in the form of a dictionary
 
@@ -32,29 +32,67 @@ memberList = {}#This is our list in the form of a dictionary
 
 
 #To add/remove to the list we will have the all card number as the key and MemberClass(obj) is the value
+#Looking up information of a club member will be done with the All Card Number
 
 
 def addToList(allCardNumber, lastName, firstName, gradYear, position, email, major, minor):
-	member = Member(allCardNumber, lastName, firstName, gradYear, position, email, major, minor)
-	memberList[allCardNumber] = member
+        member = Member(allCardNumber, lastName, firstName, gradYear, position, email, major, minor)
+        memberList[allCardNumber] = member
 
 def removeFromList(allCardNumber):
-	del memberList[allCardNumber]
+        del memberList[allCardNumber]
 
-#Looking up information of a club member will be done with the All Card Number
-
-def getLastName(allCardNUmber):
+def getLastName(allCardNumber):
+        memberLookUp = memberList[allCardNumber]
+        print "Last Name is: "
+        print memberLookUp.lastName
 
 def getFirstName(allCardNumber):
+        memberLookUp = memberList[allCardNumber]
+        print "First Name is: "
+        print memberLookUp.firstName
 
 def getGradYear(allCardNumber):
+        memberLookUp = memberList[allCardNumber]
+        print "Graduation year is: "
+        print memberLookUp.gradYear
 
 def getEmail(allCardNumber):
+        memberLookUp = memberList[allCardNumber]
+        print "Email is: "
+        print memberLookUp.email
 
 def getMajor(allCardNumber):
+        memberLookUp = memberList[allCardNumber]
+        print "Major is: "
+        print memberLookUp.major
 
 def getMinor(allCardNumber):
-        
+        memberLookUp = memberList[allCardNumber]
+        print "Minor is: "
+        print memberLookUp.minor
 
 def printList():
-	print memberList	
+        print memberList        
+
+
+#Unit test that adds one person into dictionary.
+#Expecting to print information about the member first
+#Then expecting print of the dictionary in form of [key:value] where value is a spot in memory
+#Then remove that member from dictionary, prints empty dictionary        
+class MemberTesting(unittest.TestCase):
+        
+        def testAddRemoveMember(self):
+                addToList(1234, "Cervantes", "Edwin", 2020, "Member", "cervantese@xavier.edu", "cs", "math")
+                getLastName(1234)
+                getFirstName(1234)
+                getGradYear(1234)
+                getEmail(1234)
+                getMajor(1234)
+                getMinor(1234)
+                printList()
+                removeFromList(1234)
+                printList()
+
+if __name__ == '__main__':
+        unittest.main()
