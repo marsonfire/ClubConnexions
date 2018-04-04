25027670be9e50c396814384b304c03afd40306d
schatzk <36019128+schatzkara@users.noreply.github.com>
2018-04-04 02:08:53 -0400

    improved coverage for Calendar

--- ama/CalendarTestClass.py
+++ ama/CalendarTestClass.py
@@ -10,10 +10,21 @@ class CalendarTest(unittest.TestCase):
         #self.listOfEvents = {} #{'Group Meeting': e}
 
     def testAdd(self):
-        e = Event('Group Meeting','03152018','1830',
-                  'Hailstones 19','meeting to work on project')
+        name = 'Group Meeting'
+        date = '03152018'
+        time = '1830'
+        location = 'Hailstones 19'
+        description = 'meeting to work on project'
+        e = Event()
+        e.setName(name)
+        e.setDate(date)
+        e.setTime(time)
+        e.setLocation(location)
+        e.setDescription(description)
+##        e = Event('Group Meeting','03152018','1830',
+##                  'Hailstones 19','meeting to work on project')
         self.c.addEvent(e)
-        expected = {'Group Meeting': e}
+        expected = {e.getName() : e}
         self.assertEqual(expected, self.c.listOfEvents)
 
     def testPrint(self):
@@ -42,10 +53,17 @@ class CalendarTest(unittest.TestCase):
         result = self.c.displayEvent(e)
         self.assertEqual(expected, result)
 
+
+class anotherCalendarTest(unittest.TestCase):
+    def setUp(self):
+        self.c = Calendar()
+    
     def testDelete(self):
         e = Event('Software Engineering Class','03202018','1000',
                   'Alter 208')
         self.c.addEvent(e)
+        #self.c.printList()
+        self.c.deleteEvent(e)
         expected = None
         result = self.c.printList()
         self.assertEqual(expected, result)
