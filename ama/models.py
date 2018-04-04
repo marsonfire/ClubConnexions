5aaadd4a4bca6d8ff1fdbac96da0db4b25e71cc8
Andrew <awmarsden487@gmail.com>
2018-04-03 21:16:35 -0400

    Cleaned up models and added tests

--- ama/models.py
+++ ama/models.py
@@ -9,24 +9,19 @@ class Event(models.Model):
 	eventLocation = models.CharField(max_length=200)
 	eventDescription = models.CharField(max_length=1000)
 
-	@classmethod
-	def create(cls, eventName, eventDate, eventTime, eventLocation, eventDescription):
-		e = cls(eventName, eventDate, eventTime, eventLocation, eventDescription)
-		return e
-
-	def _str_(self):
+	def getName(self):
 		return self.eventName
 
-	def _str_(self):
+	def getDate(self):
 		return self.eventDate
 
-	def _str_(self):
+	def getTime(self):
 		return self.eventTime
 
-	def _str_(self):
+	def getLocation(self):
 		return self.eventLocation
 
-	def _str_(self):
+	def getDescription(self):
 		return self.eventDescription
 
 class Officers(models.Model):
@@ -34,34 +29,23 @@ class Officers(models.Model):
 	officerLastName = models.CharField(max_length=50)
 	officerPosition = models.CharField(max_length=50)
 
-	@classmethod
-	def create(cls, officerFirstName, officerLastName, officerPosition):
-		o = cls(officerFirstName, officerLastName, officerPosition)
-		return o
-
-	def _str_(self):
+	def getFirstName(self):
 		return self.officerFirstName
 
-	def _str_(self):
+	def getLastName(self):
 		return self.officerLastName
 
-	def _str_(self):
+	def getPosition(self):
 		return self.officerPosition
 
 class Members(models.Model):
 	memberFirstName = models.CharField(max_length=50)
 	memberLastName = models.CharField(max_length=50)
-	officerPosition = models.CharField(max_length=50)
 
-	@classmethod
-	def create(cls, officerFirstName, officerLastName, officerPosition):
-		o = cls(officerFirstName, officerLastName, officerPosition)
-		return o
-
-	def _str_(self):
+	def getFirstName(self):
 		return self.memberFirstName
 
-	def _str_(self):
+	def getLastName(self):
 		return self.memberLastName
 
 
--- ama/tests.py
+++ ama/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
 
+from .models import Event
+from .models import Officers
+from .models import Members
+
 # Create your tests here.
+
+class EventModelTest(TestCase):
+
+	def test_getName(self):
+		event = Event(eventName="TEST Event")
+		self.assertEqual(event.eventName, event.getName())
+
+	def test_getDate(self):
+		event = Event(eventDate="TEST Date")
+		self.assertEqual(event.eventDate, event.getDate())
+
+	def test_getTime(self):
+		event = Event(eventTime="TEST Time")
+		self.assertEqual(event.eventTime, event.getTime())
+
+	def test_getLocation(self):
+		event = Event(eventLocation="TEST location")
+		self.assertEqual(event.eventLocation, event.getLocation())
+
+	def test_getDescription(self):
+		event = Event(eventDescription="TEST description")
+		self.assertEqual(event.eventDescription, event.getDescription())
+
+class OfficersModelTest(TestCase):
+
+	def test_getFirstName(self):
+		o = Officers(officerFirstName="firstname")
+		self.assertEqual(o.officerFirstName, o.getFirstName())
+
+	def test_getLastName(self):
+		o = Officers(officerLastName="lastname")
+		self.assertEqual(o.officerLastName, o.getLastName())
+
+	def test_getPosition(self):
+		o = Officers(officerPosition="position")
+		self.assertEqual(o.officerPosition, o.getPosition())
+
+
+class MembersModelTest(TestCase):
+
+	def test_getFirstName(self):
+		m = Members(memberFirstName="firstname")
+		self.assertEqual(m.memberFirstName, m.getFirstName())
+
+	def test_getLastName(self):
+		m = Members(memberLastName="lastname")
+		self.assertEqual(m.memberLastName, m.getLastName())
